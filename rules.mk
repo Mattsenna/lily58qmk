f03fa6cab968b0b3beadf1f46e0d39f35758aeba
Matthew Joseph <matthewjoseph@Matthews-Macbook-Pro.local>
2020-12-23 10:57:42 +0000

    massive keymap update

--- rules.mk
+++ rules.mk
@@ -3,6 +3,7 @@ RGBLIGHT_ENABLE = yes
 ENCODER_ENABLE = yes
 
 
+
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
